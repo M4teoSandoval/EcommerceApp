616c0d681a1b6a1352a5b8c5fe64ada9b3762a10
Mateo Sandoval <ksandoval712@unab.edu.co>
2025-04-25 09:49:01 -0500

    Validacion Input

--- app/src/main/java/com/mateosandoval/ecommerceapp/LoginScreen.kt
+++ app/src/main/java/com/mateosandoval/ecommerceapp/LoginScreen.kt
@@ -1,5 +1,7 @@
 package com.mateosandoval.ecommerceapp
 
+import android.app.Activity
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,19 +23,38 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
 
 @Composable
-fun LoginScreen(navController : NavController){
+fun LoginScreen(navController: NavController) {
+
+    //Estados de los inputs
+    var inputEmail by remember { mutableStateOf("") }
+    var inputPassword by remember { mutableStateOf("") }
+
+    val activity = LocalView.current.context as Activity
+
+    var isEmaiValid by remember { mutableStateOf(false) }
+    var isPasswordValid by remember { mutableStateOf(false) }
+
     Scaffold { innerPadding ->
 
         Column(
@@ -60,8 +81,10 @@ fun LoginScreen(navController : NavController){
             Spacer(modifier = Modifier.height(20.dp))
 
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = inputEmail,
+                onValueChange = {
+                    inputEmail = it
+                },
                 label = {
                     Text(
                         text = "Email",
@@ -82,8 +105,11 @@ fun LoginScreen(navController : NavController){
             Spacer(modifier = Modifier.height(10.dp))
 
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = inputPassword,
+                visualTransformation = PasswordVisualTransformation(),
+                onValueChange = {
+                    inputPassword = it
+                },
                 label = {
                     Text(
                         text = "Contraseña",
@@ -105,7 +131,19 @@ fun LoginScreen(navController : NavController){
 
             Button(
                 onClick = {
-                    navController.navigate("home")
+                    val auth = Firebase.auth
+
+                    auth.signInWithEmailAndPassword(inputEmail, inputPassword)
+                        .addOnCompleteListener() { task ->
+
+                            if (task.isSuccessful) {
+                                navController.navigate("home")
+                            } else {
+                                Log.i("Login", "Login failed")
+                            }
+                        }
+
+
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -126,8 +164,9 @@ fun LoginScreen(navController : NavController){
 
             }) {
                 Text(
-                    text= "No tienes una cuenta? Regístrate",
-                    color = Color(0xFFFF9900),)
+                    text = "No tienes una cuenta? Regístrate",
+                    color = Color(0xFFFF9900),
+                )
             }
 
 
@@ -137,8 +176,15 @@ fun LoginScreen(navController : NavController){
 
 }
 
-@Preview
-@Composable
-fun LoginScreenPreview() {
-    //LoginScreen()
+fun validateEmail(email: String): Pair<Boolean, String> {
+    return when {
+
+        email.isEmpty() -> return Pair(false, "El email no puede estar vacío")
+        !email.endsWith("@unab.edu.co") -> return Pair(
+            false,
+            "El email debe terminar con @unab.edu.co"
+        )
+        else -> return Pair(true, "")
+
+    }
 }
